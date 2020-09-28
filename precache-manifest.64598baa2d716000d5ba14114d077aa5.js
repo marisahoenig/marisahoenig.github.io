9d04086d35ed02730891375ad6ad29f3c1ad2af1
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-09-27 21:07:18 -0400

    Updates

---
+++ precache-manifest.64598baa2d716000d5ba14114d077aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42ccb50247a6d3824836",
+    "url": "/static/js/main.42ccb502.chunk.js"
+  },
+  {
+    "revision": "c0106119511758264dda",
+    "url": "/static/js/1.c0106119.chunk.js"
+  },
+  {
+    "revision": "42ccb50247a6d3824836",
+    "url": "/static/css/main.08705b1d.chunk.css"
+  },
+  {
+    "revision": "452a31042237570e9f78b4ef134c48a4",
+    "url": "/index.html"
+  }
+];
