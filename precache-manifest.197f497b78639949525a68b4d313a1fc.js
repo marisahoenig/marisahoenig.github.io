48fd1d6450149712a842a63a5c02d7d28995434e
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-09-28 23:24:40 -0400

    Updates

---
+++ precache-manifest.197f497b78639949525a68b4d313a1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30c9cf9b06c5761ad87b",
+    "url": "/static/js/main.30c9cf9b.chunk.js"
+  },
+  {
+    "revision": "28165fa93a0c19c5f5f6",
+    "url": "/static/js/1.28165fa9.chunk.js"
+  },
+  {
+    "revision": "30c9cf9b06c5761ad87b",
+    "url": "/static/css/main.3e462e22.chunk.css"
+  },
+  {
+    "revision": "986807c101991afef6dcc984436741a3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34a73fdc1a1efd531eca8af77613a14a.js"
+  "/precache-manifest.197f497b78639949525a68b4d313a1fc.js"
 );
 
 workbox.clientsClaim();
