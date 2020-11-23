8ff3c65d03594d1c5d6acbb1891e29e444e76d32
Marisa Hoenig <marisa.hoenig@gmail.com>
2020-11-22 21:22:51 -0500

    update podcast launch date

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -13,7 +13,7 @@ export default class Podcast extends Component {
                       A podcast for young professionals blossoming in the tech industry.
                       How do you get an internship? What are the different career paths in tech?
                       We'll explore all your questions and more, hearing stories of everyday young professionals to empower technologists with tools to thrive.
-                      New episodes every Monday, starting November 23.
+                      New episodes every Monday, starting November 30.
                       </p>
                       <div id="pod-embed"><iframe title="Blossoming Technologist Podcast" src="https://anchor.fm/blossoming-technologist/embed" height="102px" width="400px" frameborder="0" scrolling="no"></iframe></div>
                       <div id="pod-links">
